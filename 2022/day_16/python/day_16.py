e9e0d9d4081b1a7aa4b7903e827bb0ddd3a2bf4f
Tim Hochberg <bitsofbits@users.noreply.github.com>
2022-12-18 20:55:02 -0700

    working, more principled vesrions of day 16

--- 2022/day_16/python/day_16.py
+++ 2022/day_16/python/day_16.py
@@ -1,8 +1,7 @@
 import sys
 from time import perf_counter
 
-from implementation import (dual_max_pressure_release, max_pressure_release,
-                            parse_graph)
+from implement2 import dual_traverse, parse_graph, simplify_AA, traverse
 
 if __name__ == "__main__":
     args = sys.argv[1:]
@@ -11,10 +10,11 @@ if __name__ == "__main__":
     with open(path) as f:
         text = f.read()
 
+    nodes = simplify_AA(parse_graph(text))
+
     t0 = perf_counter()
-    nodes = parse_graph(text)
-    pressure = max_pressure_release(nodes)
+    pressure = traverse(nodes, 30)
     print(f"Part 1: Max pressure release: {pressure} ({perf_counter() - t0})")
     t0 = perf_counter()
-    freq = dual_max_pressure_release(nodes)
+    freq = dual_traverse(nodes, 26)
     print(f"Part 2: Dual max pressure release: {freq} ({perf_counter() - t0})")
--- 2022/day_16/python/implementation.py
+++ 2022/day_16/python/implementation.py
@@ -2,7 +2,23 @@ import re
 from math import inf
 from typing import NamedTuple, Tuple
 
-from numba import jit
+# def simplify_A(nodes):
+#     """
+#     >>> nodes = simplify_A(parse_graph(example_text))
+#     >>> for k in sorted(nodes): print(nodes[k])
+#     Node(label='AA', flow=0, dests=(('BB', 1), ('DD', 1), ('JJ', 2)))
+#     Node(label='BB', flow=13, dests=(('CC', 1), ('DD', 4), ('JJ', 3)))
+#     Node(label='CC', flow=2, dests=(('BB', 1), ('DD', 1)))
+#     Node(label='DD', flow=20, dests=(('BB', 4), ('CC', 1), ('EE', 1), ('JJ', 3)))
+#     Node(label='EE', flow=3, dests=(('DD', 1), ('HH', 3)))
+#     Node(label='HH', flow=22, dests=(('EE', 3),))
+#     Node(label='JJ', flow=21, dests=(('BB', 3), ('DD', 3)))
+#     """
+#     s1 = simplify(nodes, preserve={"AA"})
+#     s2 = simplify(nodes, preserve=())
+#     s2["AA"] = s1["AA"]
+#     return s2
+
 
 example_text = """
 Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
@@ -66,8 +82,118 @@ def parse_graph(text):
             g = p.match(line).groups()
             lbl = g[0]
             flow = int(g[1])
-            dests = tuple(x.strip() for x in g[2].split(","))
+            dests = tuple(set(x.strip() for x in g[2].split(",")))
             nodes[lbl] = Node(lbl, flow, dests)
+    return nodes
+
+
+def simplify(nodes, preserve={"AA"}):
+    """
+    >>> nodes = simplify(parse_graph(example_text), preserve={"AA"})
+    >>> for k in sorted(nodes): print(nodes[k])
+    Node(label='AA', flow=0, dests=(('BB', 1), ('DD', 1), ('JJ', 2)))
+    Node(label='BB', flow=13, dests=(('AA', 1), ('CC', 1)))
+    Node(label='CC', flow=2, dests=(('BB', 1), ('DD', 1)))
+    Node(label='DD', flow=20, dests=(('AA', 1), ('CC', 1), ('EE', 1)))
+    Node(label='EE', flow=3, dests=(('DD', 1), ('HH', 3)))
+    Node(label='HH', flow=22, dests=(('EE', 3),))
+    Node(label='JJ', flow=21, dests=(('AA', 2),))
+    """
+    nodes = nodes.copy()
+    for k in list(nodes):
+        nd = nodes[k]
+        dests = set((d, 1) for d in nd.dests)
+        nodes[k] = Node(nd.label, nd.flow, dests)
+    while True:
+        parents = {k: set() for k in nodes}
+        for k, nd in nodes.items():
+            for d, c in nd.dests:
+                parents[d].add((k, c))
+        for k in list(nodes):
+            nd_k = nodes[k]
+            if nd_k.flow == 0 and k not in preserve:
+                for p, c0 in parents[k]:
+                    nd_p = nodes[p]
+                    new_dests = {d: c for (d, c) in nd_p.dests if d != k}
+                    for d, c in nd_k.dests:
+                        if d == p:
+                            continue
+                        cd = c + c0
+                        if d in new_dests:
+                            cd = min(cd, new_dests[d])
+                        new_dests[d] = cd
+                    nodes[p] = Node(nd_p.label, nd_p.flow, set(new_dests.items()))
+                del nodes[k]
+                break
+        else:
+            break
+    for k in list(nodes):
+        nd = nodes[k]
+        assert nd.label == k
+        dests = tuple(sorted(nd.dests))
+        nodes[k] = Node(nd.label, nd.flow, dests)
+    return nodes
+
+
+def simplify_A(nodes):
+    """
+    >>> nodes = simplify_A(parse_graph(example_text))
+    >>> for k in sorted(nodes): print(nodes[k])
+    Node(label='AA', flow=0, dests=(('BB', 1), ('DD', 1), ('JJ', 2)))
+    Node(label='BB', flow=13, dests=(('CC', 1), ('DD', 4), ('JJ', 3)))
+    Node(label='CC', flow=2, dests=(('BB', 1), ('DD', 1)))
+    Node(label='DD', flow=20, dests=(('BB', 4), ('CC', 1), ('EE', 1), ('JJ', 3)))
+    Node(label='EE', flow=3, dests=(('DD', 1), ('HH', 3)))
+    Node(label='HH', flow=22, dests=(('EE', 3),))
+    Node(label='JJ', flow=21, dests=(('BB', 3), ('DD', 3)))
+    """
+    s1 = simplify(nodes, preserve={"AA"})
+    s2 = simplify(nodes, preserve=())
+    s2["AA"] = s1["AA"]
+    return s2
+
+
+def stretch(nodes):
+    """stretch simplified graph by adding dummy nodes
+
+    >>> nodes = stretch(simplify(parse_graph(example_text)))
+    >>> for k in sorted(nodes): print(nodes[k])
+    Node(label='AA', flow=0, dests=('AAJJ1', 'BB', 'DD'))
+    Node(label='AAJJ1', flow=0, dests=('JJ',))
+    Node(label='BB', flow=13, dests=('AA', 'CC'))
+    Node(label='CC', flow=2, dests=('BB', 'DD'))
+    Node(label='DD', flow=20, dests=('AA', 'CC', 'EE'))
+    Node(label='EE', flow=3, dests=('DD', 'EEHH1'))
+    Node(label='EEHH1', flow=0, dests=('EEHH2',))
+    Node(label='EEHH2', flow=0, dests=('HH',))
+    Node(label='HH', flow=22, dests=('HHEE1',))
+    Node(label='HHEE1', flow=0, dests=('HHEE2',))
+    Node(label='HHEE2', flow=0, dests=('EE',))
+    Node(label='JJ', flow=21, dests=('JJAA1',))
+    Node(label='JJAA1', flow=0, dests=('AA',))
+    """
+    nodes = nodes.copy()
+    # Convert to new format?
+    for k in list(nodes):
+        nd_k = nodes[k]
+        new_dests = set()
+        for d, c in nd_k.dests:
+            if c == 1:
+                new_dests.add(d)
+            else:
+                k1 = f"{k}{d}{1}"
+                new_dests.add(k1)
+                for i in range(1, c - 1):
+                    k2 = f"{k}{d}{i + 1}"
+                    nodes[k1] = Node(k1, 0, {k2})
+                    k1 = k2
+                nodes[k1] = Node(k1, 0, {d})
+        nodes[k] = Node(nd_k.label, nd_k.flow, new_dests)
+    for k in list(nodes):
+        nd = nodes[k]
+        assert nd.label == k
+        dests = tuple(sorted(nd.dests))
+        nodes[k] = Node(nd.label, nd.flow, dests)
     return nodes
 
 
@@ -124,54 +250,63 @@ def traverse_from(label, nodes, opened, time_left, states):
     return released
 
 
+class UpperBound(int):
+    pass
+
+
+def upper_bound(time_left, lbl1, lbl2, nodes, opened):
+    remaining = [x.flow for (k, x) in nodes.items() if k not in opened]
+    remaining.sort(reverse=True)
+    return sum(
+        x * max(time_left - 2 * i, 0) for (i, x) in enumerate(remaining[0::2])
+    ) + sum(x * max(time_left - 2 * i, 0) for (i, x) in enumerate(remaining[1::2]))
+
+
 def dual_traverse_from(labels, nodes, opened, time_left, states, score, scores):
     """traverse graph withing time_limit
 
-    2176 is too low :-(
-
-    >>> nodes = parse_graph(example_text)
-    >>> opened = {k for k in nodes if nodes[k].flow == 0}
-    >>> scores = {k : 0 for k in range(0, 27)}
-    >>> released = dual_traverse_from(("AA", "AA"), nodes, opened, 26, {}, 0, scores)
-    >>> released
-    1707
+    # >>> nodes = parse_graph(example_text)
+    # >>> opened = {k for k in nodes if nodes[k].flow == 0}
+    # >>> scores = {k : 0 for k in range(0, 27)}
+    # >>> released = dual_traverse_from(("AA", "AA"), nodes, opened, 26, {}, 0, scores)
+    # >>> released
+    # 1707
     """
     time_left -= 1
-    if time_left <= 0 or len(opened) >= len(nodes):
+    if time_left < 0:
         return score
+    scores[time_left] = max(score, scores[time_left])
 
     state_key = (time_left, frozenset(labels), frozenset(opened))
     if state_key in states:
-        return score + states[state_key]
+        remaining = states[state_key]
+        if isinstance(remaining, UpperBound):
+            if score + remaining < scores[time_left]:
+                return UpperBound(score + remaining)
+        else:
+            return score + remaining
 
     lbl1, lbl2 = labels
     nd1, nd2 = nodes[lbl1], nodes[lbl2]
-    if time_left == 1:
-        # No sense traversing since we can't turn things on in time
-        cls1 = lbl1 not in opened
-        cls2 = lbl2 not in opened
-        remaining = (cls1 * nd1.flow + cls2 * nd2.flow) * time_left
-        states[state_key] = remaining
-        return remaining
+    # if time_left == 1:
+    #     # No sense traversing since we can't turn things on in time
+    #     cls1 = lbl1 not in opened
+    #     cls2 = lbl2 not in opened
+    #     remaining = (cls1 * nd1.flow + cls2 * nd2.flow) * time_left
+    #     states[state_key] = remaining
+    #     return remaining
 
-    remaining = [x.flow for (k, x) in nodes.items() if k not in opened]
-    remaining.sort(reverse=True)
-    max_left = 0
-    tl = time_left
-    for i, x in enumerate(remaining):
-        max_left += x * tl
-        if i % 2:
-            tl = max(tl - 2, 0)
-    if score + max_left <= scores[time_left]:
-        return 0  # give up
+    max_left = upper_bound(time_left, lbl1, lbl2, nodes, opened)
+    if score + max_left < scores[time_left]:
+        # No way to get the best score, so give up
+        states[state_key] = UpperBound(max_left)
+        return UpperBound(score + max_left)
 
-    scores[time_left] = max(score, scores[time_left])
-
-    released = None
+    released = 0
     for d1, opn1 in [(x, False) for x in nd1.dests] + [(lbl1, True)]:
         for d2, opn2 in [(x, False) for x in nd2.dests] + [(lbl2, True)]:
 
-            if (lbl1 == lbl2) and (opn1 is opn2 is True):
+            if lbl1 == lbl2 and opn1 is opn2 is True:
                 # Can only open a valve once
                 continue
             if (opn1 and (lbl1 in opened)) or (opn2 and (lbl2 in opened)):
@@ -193,12 +328,55 @@ def dual_traverse_from(labels, nodes, opened, time_left, states, score, scores):
                 score=score + lcl_score,
                 scores=scores,
             )
-            if released is None or new_r > released:
+            if new_r > released:
                 released = new_r
 
-    assert released is not None
-    states[state_key] = released - score
+    if isinstance(released, UpperBound):
+        states[state_key] = UpperBound(released - score)
+    else:
+        states[state_key] = released - score
     return released
+
+
+def dual_traverse(graph, start, max_time):
+    """
+    # >>> nodes = parse_graph(example_text)
+    # >>> released = dual_traverse(nodes, ("AA", "AA"), 26)
+    # >>> released
+    """
+    # Process nodes into simpler form
+    fast_nodes = {}
+    for k, nd in graph.items():
+        fast_nodes[k] = tuple((d, 0) for d in nd.dests) + ((k, nd.flow),)
+
+    k1, k2 = start
+    assert k1 == "AA" and k2 == "AA"
+    stack = [(max_time, k1, k2, 0, fast_nodes)]
+    states = {}
+    max_score = 0
+    while stack:
+        time_left, k1, k2, score, nodes = stack.pop()
+        state_key = (time_left, k1, k2, tuple(sorted(nodes.items())))
+        states[state_key] = score  # how to get this into a useful form
+        max_score = max(score, max_score)
+        time_left -= 1
+        if time_left == 0:
+            continue
+        for d1, flow_1 in nodes[k1]:
+            for d2, flow_2 in nodes[k2]:
+                if flow_1 != 0 or flow_2 != 0:
+                    nodes = nodes.copy()
+                    if flow_1 != 0:
+                        nodes[k1] = tuple(
+                            (dx, 0 if dx == d1 else fx) for (dx, fx) in nodes[k1]
+                        )
+                    if flow_2 != 0:
+                        nodes[k2] = tuple(
+                            (dx, 0 if dx == d1 else fx) for (dx, fx) in nodes[k2]
+                        )
+                next_score = score + (flow_1 + flow_2) * time_left
+                stack.append((time_left, d1, d2, next_score, nodes))
+    return max_score
 
 
 def max_pressure_release(nodes):
@@ -207,19 +385,23 @@ def max_pressure_release(nodes):
     >>> max_pressure_release(nodes)
     1651
     """
+    nodes = stretch(simplify(nodes))
     opened = {k for k in nodes if nodes[k].flow == 0}
     return traverse_from("AA", nodes, opened, 30, {})
 
 
 def dual_max_pressure_release(nodes):
     """
-    >>> nodes = parse_graph(example_text)
-    >>> dual_max_pressure_release(nodes)
-    1707
+    # >>> nodes = parse_graph(example_text)
+    # >>> dual_max_pressure_release(nodes)
+    # 1707
     """
+    nodes = stretch(simplify(nodes))
     opened = {k for k in nodes if nodes[k].flow == 0}
     scores = {k: 0 for k in range(0, 27)}
-    return dual_traverse_from(("AA", "AA"), nodes, opened, 26, {}, 0, scores)
+    released = dual_traverse_from(("AA", "AA"), nodes, opened, 26, {}, 0, scores)
+    assert not isinstance(released, UpperBound)
+    return released
 
 
 if __name__ == "__main__":
