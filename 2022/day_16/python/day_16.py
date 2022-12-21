828ebcd7c1d90e0e293a71a9aa61ee465e861bbd
Tim Hochberg <bitsofbits@users.noreply.github.com>
2022-12-21 08:46:05 -0700

    day 16: apply trick where you use memoization from single traverse to solve pt 1 (from reddit)

--- 2022/day_16/python/day_16.py
+++ 2022/day_16/python/day_16.py
@@ -8,15 +8,16 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     (path,) = args
 
+    t0 = perf_counter()
     with open(path) as f:
         text = f.read()
-
     nodes = simplify(parse_graph(text))
     add_paths(nodes)
+    print(f"Setting up graph took ({perf_counter() - t0:.2}s)")
 
     t0 = perf_counter()
     pressure = traverse(nodes, 30)
-    print(f"Part 1: Max pressure release: {pressure} ({perf_counter() - t0})")
+    print(f"Part 1: Max pressure release: {pressure} ({perf_counter() - t0:.2}s)")
     t0 = perf_counter()
     freq = dual_traverse(nodes, 26)
-    print(f"Part 2: Dual max pressure release: {freq} ({perf_counter() - t0})")
+    print(f"Part 2: Dual max pressure release: {freq} ({perf_counter() - t0:.2}s)")
--- 2022/day_16/python/implementation.py
+++ 2022/day_16/python/implementation.py
@@ -118,25 +118,6 @@ def simplify(nodes, preserve={"AA"}, preserve_nonzero=True):
     return nodes
 
 
-def simplify_AA(nodes):
-    """Simplify away the AA node, then add back in the connections from AA
-
-    >>> nodes = simplify_AA(parse_graph(example_text))
-    >>> for k in sorted(nodes): print(nodes[k])
-    Node(label='AA', flow=0, dests=(('BB', 1), ('DD', 1), ('JJ', 2)))
-    Node(label='BB', flow=13, dests=(('CC', 1), ('DD', 2), ('JJ', 3)))
-    Node(label='CC', flow=2, dests=(('BB', 1), ('DD', 1)))
-    Node(label='DD', flow=20, dests=(('BB', 2), ('CC', 1), ('EE', 1), ('JJ', 3)))
-    Node(label='EE', flow=3, dests=(('DD', 1), ('HH', 3)))
-    Node(label='HH', flow=22, dests=(('EE', 3),))
-    Node(label='JJ', flow=21, dests=(('BB', 3), ('DD', 3)))
-    """
-    s1 = simplify(nodes, preserve={"AA"})
-    s2 = simplify(nodes, preserve=())
-    s2["AA"] = s1["AA"]
-    return s2
-
-
 def _merge(d1, d2, k):
     dmap = dict(d1)
     for d, c in d2:
@@ -151,13 +132,13 @@ def add_paths(nodes):
     >>> nodes = simplify(parse_graph(example_text), preserve={"AA"})
     >>> add_paths(nodes)
     >>> for k in sorted(nodes): print(nodes[k])
-    Node(label='AA', flow=0, dests=(('BB', 1), ('CC', 2), ('DD', 1), ('EE', 2), ('HH', 5), ('JJ', 2)))
-    Node(label='BB', flow=13, dests=(('AA', 1), ('CC', 1), ('DD', 2), ('EE', 3), ('HH', 6), ('JJ', 3)))
-    Node(label='CC', flow=2, dests=(('AA', 2), ('BB', 1), ('DD', 1), ('EE', 2), ('HH', 5), ('JJ', 4)))
-    Node(label='DD', flow=20, dests=(('AA', 1), ('BB', 2), ('CC', 1), ('EE', 1), ('HH', 4), ('JJ', 3)))
-    Node(label='EE', flow=3, dests=(('AA', 2), ('BB', 3), ('CC', 2), ('DD', 1), ('HH', 3), ('JJ', 4)))
-    Node(label='HH', flow=22, dests=(('AA', 5), ('BB', 6), ('CC', 5), ('DD', 4), ('EE', 3), ('JJ', 7)))
-    Node(label='JJ', flow=21, dests=(('AA', 2), ('BB', 3), ('CC', 4), ('DD', 3), ('EE', 4), ('HH', 7)))
+    Node(label='AA', flow=0, dests=[('BB', 1), ('CC', 2), ('DD', 1), ('EE', 2), ('HH', 5), ('JJ', 2)])
+    Node(label='BB', flow=13, dests=[('AA', 1), ('CC', 1), ('DD', 2), ('EE', 3), ('HH', 6), ('JJ', 3)])
+    Node(label='CC', flow=2, dests=[('AA', 2), ('BB', 1), ('DD', 1), ('EE', 2), ('HH', 5), ('JJ', 4)])
+    Node(label='DD', flow=20, dests=[('AA', 1), ('BB', 2), ('CC', 1), ('EE', 1), ('HH', 4), ('JJ', 3)])
+    Node(label='EE', flow=3, dests=[('AA', 2), ('BB', 3), ('CC', 2), ('DD', 1), ('HH', 3), ('JJ', 4)])
+    Node(label='HH', flow=22, dests=[('AA', 5), ('BB', 6), ('CC', 5), ('DD', 4), ('EE', 3), ('JJ', 7)])
+    Node(label='JJ', flow=21, dests=[('AA', 2), ('BB', 3), ('CC', 4), ('DD', 3), ('EE', 4), ('HH', 7)])
     """
     labels = list(nodes)
     for a in labels:
@@ -166,155 +147,113 @@ def add_paths(nodes):
                 continue
             simple = simplify(nodes, preserve={a, b}, preserve_nonzero=False)
             A = nodes[a]
-            dests = _merge(A.dests, simple[a].dests, b)
-            nodes[a] = Node(label=a, flow=A.flow, dests=dests)
+            A_dests = _merge(A.dests, simple[a].dests, b)
+            nodes[a] = Node(label=a, flow=A.flow, dests=A_dests)
             B = nodes[b]
-            dests = _merge(B.dests, simple[b].dests, a)
-            nodes[b] = Node(label=b, flow=B.flow, dests=dests)
-    for a in labels:
-        assert len(nodes[a].dests) == len(labels) - 1, (a, b, len(labels), len(dests))
-        for d, c in nodes[a].dests:
-            import math
-
-            assert c > 0
-            assert not math.isinf(c)
+            B_dests = _merge(B.dests, simple[b].dests, a)
+            nodes[b] = Node(label=b, flow=B.flow, dests=B_dests)
 
 
 def setup_nodes(graph):
     nodes = {}
+    # We don't need AA in the final graph, just as starting points, so remove now.
+    starts = graph["AA"].dests
     for k, nd in graph.items():
-        nodes[k] = (nd.flow, tuple((d, c) for (d, c) in nd.dests))
-    opened = frozenset(k for (k, v) in graph.items() if v.flow == 0)
-    return nodes, opened
+        if k == "AA":
+            continue
+        nodes[k] = (nd.flow, tuple((d, c) for (d, c) in nd.dests if d != "AA"))
+    opened = frozenset(k for (k, v) in graph.items() if v.flow == 0 and k != "AA")
+    return starts, nodes, opened
 
 
-def _traverse(lbl, nodes, time_left, opened, states):
-    if time_left <= 1 or len(opened) == len(nodes):
-        return 0
-
-    key = (time_left, lbl, opened)
-
-    if key in states:
-        return states[key]
+def core(lbl, nodes, time_left, opened, score, score_map):
+    if time_left < 2 or len(opened) == len(nodes):
+        score_map[opened] = max(score, score_map.get(opened, 0))
+        return score
 
     flow, dests = nodes[lbl]
-
-    lcl_score = flow * (time_left - 1) * (lbl not in opened)
-    released = lcl_score
+    lcl_score = score + flow * (time_left - 1) * (lbl not in opened)
 
     opened = frozenset(opened | {lbl})
+    score_map[opened] = max(lcl_score, score_map.get(opened, 0))
+
+    result = lcl_score
     for (d, c) in dests:
         if d not in opened and time_left - c > 2:
-            released = max(
-                released,
-                _traverse(d, nodes, time_left - c - 1, opened, states) + lcl_score,
+            result = max(
+                result,
+                core(d, nodes, time_left - c - 1, opened, lcl_score, score_map),
             )
 
-    states[key] = released
-    return released
+    return result
 
 
-def traverse(graph, time_left):
+def traverse_mp(nd, nodes, time_left, opened):
+    score_map = {}
+    released = core(nd, nodes, time_left, opened, 0, score_map)
+    return released, score_map
+
+
+def traverse(graph, time_left, return_map=False):
     """traverse graph within time_limit
 
     >>> nodes = simplify(parse_graph(example_text))
     >>> add_paths(nodes)
     >>> traverse(nodes, 30)
     1651
+
+    2286
     """
-    nodes, opened = setup_nodes(graph)
-    released = 0
-    _, dests = nodes["AA"]
+    starts, nodes, opened = setup_nodes(graph)
+    score = 0
+    score_map = {}
     with ProcessPoolExecutor() as exe:
         futures = []
-        for nd, cost in dests:
-            f = exe.submit(_traverse, nd, nodes, time_left - cost, opened, states={})
+        for nd, cost in starts:
+            f = exe.submit(traverse_mp, nd, nodes, time_left - cost, opened)
             futures.append(f)
         for f in as_completed(futures):
-            released = max(released, f.result())
-    return released
-
-
-def _dispatch(lbl, other_lbl_tl, nodes, time_left, opened, states):
-    flow, dests = nodes[lbl]
-    olbl, otl = other_lbl_tl
-
-    flow, dests = nodes[lbl]
-    lcl_score = flow * (time_left - 1)
-
-    propogated = False
-    opened_copy = None
-    released = lcl_score
-    for (d, c) in dests:
-        if d in opened or d == olbl:
-            continue
-        dtl = time_left - 1 - c
-        if dtl > 1:
-            if opened_copy is None:
-                opened_copy = frozenset(opened | {lbl})
-            propogated = True
-            lbl_tls = ((d, dtl), other_lbl_tl)
-            released = max(
-                released,
-                _dual_traverse(lbl_tls, nodes, max(otl, dtl), opened_copy, states)
-                + lcl_score,
-            )
-    if not propogated and otl > 1:
-        # Allow other agent to run
-        lbl_tls = (other_lbl_tl, (lbl, -1))
-        released = max(
-            released, _dual_traverse(lbl_tls, nodes, otl, opened, states) + lcl_score
-        )
-    return released
-
-
-def _dual_traverse(lbl_tls, nodes, time_left, opened, states):
-    if time_left <= 1 or len(opened) == len(nodes):
-        return 0
-
-    key = (time_left, lbl_tls, opened)
-    if key in states:
-        return states[key]
-
-    (lbl1, t1), (lbl2, t2) = lbl_tls
-    if t1 == time_left:
-        released = _dispatch(lbl1, (lbl2, t2), nodes, time_left, opened, states)
-    elif t2 == time_left:
-        released = _dispatch(lbl2, (lbl1, t1), nodes, time_left, opened, states)
+            r, m = f.result()
+            score = max(score, r)
+            for k, v in m.items():
+                score_map[k] = max(v, score_map.get(k, 0))
+    if return_map:
+        return score_map
     else:
-        raise RuntimeError("should not get here")
+        return score
 
-    states[key] = released
-    return released
+
+def int_key(kset, kmap):
+    key = 0
+    for k in kset:
+        key |= kmap[k]
+    return key
 
 
 def dual_traverse(graph, time_left):
-    """traverse graph within time_limit with two actors
-
+    """
     >>> nodes = simplify(parse_graph(example_text))
     >>> add_paths(nodes)
     >>> dual_traverse(nodes, 26)
     1707
     """
-    nodes, opened = setup_nodes(graph)
-    released = 0
-    _, dests = nodes["AA"]
-    with ProcessPoolExecutor() as exe:
-        futures = []
-        for nd_a, cost_a in dests:
-            for nd_b, cost_b in dests:
-                if nd_b <= nd_a:
-                    continue
-                dtls = ((nd_a, time_left - cost_a), (nd_b, time_left - cost_b))
-                t = time_left - min(cost_a, cost_b)
-                f = exe.submit(_dual_traverse, dtls, nodes, t, opened, states={})
-                futures.append(f)
-        for f in as_completed(futures):
-            released = max(released, f.result())
-    return released
+    score_map = traverse(graph, time_left, return_map=True)
+    # Convert keys to bitfields so comparisons are faster
+    keys = set()
+    for k in score_map:
+        keys |= k
+    kmap = {k: 1 << i for (i, k) in enumerate(keys)}
+    score_map = {int_key(k, kmap): v for (k, v) in score_map.items()}
+    # Try all compatible combinations, meaning they didn't both turn
+    # on a valve
+    best = 0
+    keys = list(score_map)
+    for i, k1 in enumerate(keys):
+        for k2 in keys[i + 1 :]:
+            if not k1 & k2:
+                best = max(best, score_map[k1] + score_map[k2])
 
-    lbl_ts = (("AA", time_left),) * 2
-    return _dual_traverse(lbl_ts, nodes, time_left, opened, states={})
+    return best
 
 
 if __name__ == "__main__":
