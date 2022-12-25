81861136f142e147ac3d8d8a1ba8364132b21e34
Tim Hochberg <bitsofbits@users.noreply.github.com>
2022-12-25 08:04:09 -0700

    day 25: clean up a bit

--- 2022/day_25/python/implementation.py
+++ 2022/day_25/python/implementation.py
@@ -1,4 +1,4 @@
-from itertools import count
+from collections import deque
 
 example_text = """
 1=-0-2
@@ -28,24 +28,7 @@ SNAFU = {
 DECIMAL = {v: k for (k, v) in SNAFU.items()}
 
 
-B5 = {0: [0, 0], 1: [0, 1], 2: [0, 2], 3: [1, -2], 4: [1, -1]}
-
-
-"""
-1=-0-2     1747
- 12111      906
-  2=0=      198
-    21       11
-  2=01      201
-   111       31
- 20012     1257
-   112       32
- 1=-1=      353
-  1-12      107
-    12        7
-    1=        3
-   122       37
-"""
+BASE5_TO_BALANCED = {0: [0, 0], 1: [0, 1], 2: [0, 2], 3: [1, -2], 4: [1, -1]}
 
 
 def SNAFU_to_decimal(line):
@@ -64,23 +47,20 @@ def SNAFU_to_decimal(line):
     return value
 
 
-def base_5(n):
+def reversed_base_5(n):
     if n == 0:
         return [0]
-    digits = []
     while n:
-        digits.append(int(n % 5))
+        yield n % 5
         n //= 5
-    return digits[::-1]
 
 
 def decimal_to_SNAFU(n):
-    b5 = base_5(n)
-    digits = [0] * (len(b5) + 1)
-    n = len(b5)
-    for i, b in enumerate(b5[::-1]):
-        s1, s0 = B5[b]
-        d = digits[n - i] + s0
+    digits = deque([0])
+    for i, b in enumerate(reversed_base_5(n)):
+        digits.appendleft(0)
+        s1, s0 = BASE5_TO_BALANCED[b]
+        d = digits[1] + s0
         match d:
             case 4:
                 c = 1
@@ -96,11 +76,11 @@ def decimal_to_SNAFU(n):
                 d = 1
             case _:
                 c = 0
-        digits[n - i] = d
-        digits[n - (i + 1)] = s1 + c
+        digits[1] = d
+        digits[0] = s1 + c
     while digits[0] == 0:
-        digits = digits[1:]
-    return "".join([DECIMAL[x] for x in digits])
+        digits.popleft()
+    return "".join(DECIMAL[x] for x in digits)
 
 
 def part_1(text):
