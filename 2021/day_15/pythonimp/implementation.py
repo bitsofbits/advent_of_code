2be69e1b793c9aa9462118e31726084776b54f31
Tim Hochberg <bitsofbits@users.noreply.github.com>
2022-12-31 02:36:50 -0700

    2021, day 15: working version but slow (18s) than I like

--- 2021/day_15/pythonimp/implementation.py
+++ 2021/day_15/pythonimp/implementation.py
@@ -1,3 +1,5 @@
+from collections import deque
+from functools import cache
 from math import inf
 
 EXAMPLE_TEXT = """
@@ -27,38 +29,80 @@ def parse(text):
     return board
 
 
+# class ScoreEstimator:
+#     """
+#     >>> board = parse(EXAMPLE_TEXT)
+#     >>> scorer = ScoreEstimator(board)
+#     >>> scorer.pessimistic(9, 9)
+#     0
+#     >>> scorer.pessimistic(0, 0)
+#     """
+
+#     def __init__(self, board):
+#         self.board = board
+#         self.end = (max(i for (i, j) in board), max(j for (i, j) in board))
+
+#     @cache
+#     def pessimistic(self, i, j):
+#         score = 0
+#         i1, j1 = self.end
+#         while (i, j) != self.end:
+#             di, dj = (i1 - i), (j1 - j)
+#             if di > dj:
+#                 i += 1
+#             else:
+#                 j += 1
+#             if (i)
+#             score += self.board[i, j]
+#         return score
+
+
 def traverse(board):
     start = (0, 0)
     end = (max(i for (i, j) in board), max(j for (i, j) in board))
 
-    def optimistic_score(x):
-        s, (i, j), *_ = x
-        return s + (end[0] - i) + (end[1] - j)
+    # scorer = ScoreEstimator(board)
 
-    def pessimistic_score(x):
-        s, (i, j), *_ = x
-        return s + 9 * (end[0] - i) + 9 * (end[1] - j)
+    def optimistic_score(s, i, j):
+        return s + ((end[0] - i) + (end[1] - j))
 
-    stack = [(0, start, set())]
+    def pessimistic_score(s, i, j):
+        return s + 9 * ((end[0] - i) + (end[1] - j))
+
+    stack = [(sum(end), 0, start, set())]
     scores = {}
-    best_score = inf
+    best_score = 9 * sum(end)
+
+    assert end[0] == end[1]
+
     while stack:
-        stack.sort(key=optimistic_score)
-        score, (i, j), visited = stack.pop()
-        for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        stack.sort(reverse=True)
+        _, score, (i, j), visited = stack.pop()
+        for di, dj in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
             k1 = i + di, j + dj
             if k1 in board and k1 not in visited:
                 next_score = score + board[k1]
-                good_score = optimistic_score((next_score, k1))
-                if good_score < best_score and score < scores.get(k1, inf):
-                    scores[k1] = score
-                    bad_score = pessimistic_score((next_score, k1))
-                    best_score = min(best_score, bad_score)
-                    if k1 != end:
-                        stack.append((next_score, k1, visited | {k1}))
-                    else:
-                        print(next_score)
+                if next_score < scores.get(k1, inf):
+                    scores[k1] = next_score
+                    good_score = optimistic_score(next_score, *k1)
+                    if good_score < best_score:
+                        best_score = min(best_score, pessimistic_score(next_score, *k1))
+                        if k1 != end:
+                            stack.append((good_score, next_score, k1, visited | {k1}))
     return best_score
+
+
+def build_big_board(board, n):
+    di = max(i for (i, j) in board) + 1
+    dj = max(j for (i, j) in board) + 1
+    big_board = {}
+    for a in range(n):
+        for b in range(n):
+            for k, c in board.items():
+                i, j = k
+                c = (c + a + b - 1) % 9 + 1
+                big_board[i + a * di, j + b * dj] = c
+    return big_board
 
 
 def part_1(text):
@@ -73,7 +117,13 @@ def part_1(text):
 def part_2(text):
     """
     >>> part_2(EXAMPLE_TEXT)
+    315
     """
+    small_board = parse(text)
+    board = build_big_board(small_board, 5)
+    return traverse(board)
+
+    # 3688 too high
 
 
 if __name__ == "__main__":
