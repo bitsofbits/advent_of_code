bea3bfeb48ee271407600f4a985119d5cd0c3d6f
Tim Hochberg <bitsofbits@users.noreply.github.com>
2023-01-01 10:16:40 -0700

    2021, day 20: use convolve twice to get run time for each problem below 1s

--- 2021/day_20/pythonimp/implementation.py
+++ 2021/day_20/pythonimp/implementation.py
@@ -104,11 +104,67 @@ def convolve(filt, img, extent, invert=False):
         k0 = (i - min_i + 2) * W - (min_j - 2)
         for j in range(min_j - 1, max_j + 2):
             k = k0 + j
-            x = 0
+            ndx = 0
             for os in offsets:
-                x = 2 * x + raster[k + os]
-            new_img[i, j] = filt[x]
+                ndx = 2 * ndx + raster[k + os]
+            new_img[i, j] = filt[ndx]
     return new_img
+
+
+def convolve_twice(filt, img, extent):
+    """
+    >>> filt, img = parse(EXAMPLE_TEXT)
+    >>> extent = compute_extent(img)
+    >>> img = convolve(filt, img, extent)
+    >>> print(render(img))
+    |.##.##.|
+    |#..#.#.|
+    |##.#..#|
+    |####..#|
+    |.#..##.|
+    |..##..#|
+    |...#.#.|
+    >>> img = convolve(filt, img, pad_extent(extent))
+    >>> print(render(img))
+    |.......#.|
+    |.#..#.#..|
+    |#.#...###|
+    |#...##.#.|
+    |#.....#.#|
+    |.#.#####.|
+    |..#.#####|
+    |...##.##.|
+    |....###..|
+    """
+    min_i, max_i, min_j, max_j = extent
+    H = max_i - min_i + 7
+    W = max_j - min_j + 7
+    raster = [0] * (H * W)
+    for (i, j), v in img.items():
+        raster[(i - min_i + 3) * W + (j - min_j + 3)] = v
+    offsets = [di * W + dj for di in [-1, 0, 1] for dj in [-1, 0, 1]]
+
+    new_raster = [filt[0] == 1] * (H * W)
+    for i in range(min_i - 1, max_i + 2):
+        k0 = (i - min_i + 3) * W - (min_j - 3)
+        for j in range(min_j - 1, max_j + 2):
+            k = k0 + j
+            ndx = 0
+            for os in offsets:
+                ndx = 2 * ndx + raster[k + os]
+            new_raster[k] = filt[ndx]
+
+    img = {}
+
+    for i in range(min_i - 2, max_i + 3):
+        k0 = (i - min_i + 3) * W - (min_j - 3)
+        for j in range(min_j - 2, max_j + 3):
+            k = k0 + j
+            ndx = 0
+            for os in offsets:
+                ndx = 2 * ndx + new_raster[k + os]
+            img[i, j] = filt[ndx]
+    return img
 
 
 def pad_extent(extent, n=1):
@@ -120,18 +176,10 @@ def part_1(text):
     """
     >>> part_1(EXAMPLE_TEXT)
     35
-
-    5698 too high
-    5633 too high
-
-    5563?
     """
     filt, img = parse(text)
-    invert = filt[0] == 1
     extent = compute_extent(img)
-    img = convolve(filt, img, extent)
-    extent = pad_extent(extent, 1)
-    img = convolve(filt, img, extent, invert)
+    img = convolve_twice(filt, img, extent)
     return sum(img.values())
 
 
@@ -141,13 +189,10 @@ def part_2(text):
     3351
     """
     filt, img = parse(text)
-    invert = filt[0] == 1
     extent = compute_extent(img)
     for i in range(25):
-        img = convolve(filt, img, extent)
-        extent = pad_extent(extent, 1)
-        img = convolve(filt, img, extent, invert)
-        extent = pad_extent(extent, 1)
+        img = convolve_twice(filt, img, extent)
+        extent = pad_extent(extent, 2)
     return sum(img.values())
 
 
