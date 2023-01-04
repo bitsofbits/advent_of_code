f6dc31fcc0c5ac0d95c72512fe762c6570d43911
Tim Hochberg <bitsofbits@users.noreply.github.com>
2023-01-03 18:54:55 -0700

    2021, day 24: fast enough but needs a once over

--- 2021/day_24/pythonimp/implementation.py
+++ 2021/day_24/pythonimp/implementation.py
@@ -65,13 +65,11 @@ class ALU:
                     self.reg[a] = inputs.popleft()
                 case "add", a, b:
                     self.reg[a] += self.reg.get(b, b)
-                    # Value.add(self.reg[a], self.reg.get(b, b))
                 case "mul", a, b:
                     if b == 0:
                         self.reg[a] = 0
                     else:
                         self.reg[a] *= self.reg.get(b, b)
-                    # Value.mul(self.reg[a], self.reg.get(b, b))
                 case "div", a, b:
                     self.reg[a] = int(self.reg[a] / self.reg.get(b, b))
                     # Value.div(self.reg[a], self.reg.get(b, b))
@@ -95,6 +93,58 @@ def split_into_subprograms(program):
     yield program[i0:]
 
 
+# inp w
+# mul x 0
+# add x z
+# mod x 26
+# div z 1 # a
+# add x 14 # b
+# eql x w
+# eql x 0
+# mul y 0
+# add y 25
+# mul y x
+# add y 1
+# mul z y
+# mul y 0
+# add y w
+# add y 8 # c
+# mul y x
+# add z y
+
+
+def extract_fast_subprogram(prog):
+    avals = []
+    bvals = []
+    cvals = []
+    for inst in prog:
+        match inst:
+            case "div", "z", int(a):
+                avals.append(a)
+            case "add", "x", int(b):
+                bvals.append(b)
+            case "add", "y", int(c):
+                cvals.append(c)
+    [a] = avals
+    [b] = bvals
+    [_, _, c] = cvals
+    return a, b, c
+
+
+def run_fast_subprogram(program, z_values):
+    a, b, c = program
+    for w in range(1, 10):
+        for z in z_values:
+            z0 = z
+            x = (z % 26 + b) != w
+            z = int(z / a)
+            y = 25 * x + 1
+            z *= y
+            y = (w + c) * x
+            z += y
+            yield (w, z0), z
+
+
 def run_subprogram(program, z_values):
     alu = ALU()
     for w in range(1, 10):
@@ -104,42 +154,23 @@ def run_subprogram(program, z_values):
             yield ((w, z), alu.reg["z"])
 
 
-def find_key(subprog):
-    vals = []
-    divs = []
-    for inst in subprog:
-        match inst:
-            case "add", "x", int(k):
-                vals.append(k)
-            case "div", "z", int(k):
-                divs.append(k)
-    [val] = vals
-    [div] = divs
-    return val, div
-
-
-# z -> z + f(v, z % 26) * z
-# z % 26 - 6 %= w
-
-
 def part_1(text):
     """
     >>> part_1(INPUT_TEXT)
+    79997391969649
     """
+    global z_cands
     full_program = parse(text)
     subprograms = list(split_into_subprograms(full_program))
-    print(find_key(subprograms[-1]))  # -> -6, 26
     # ->
     z_values = [0]
     omaps = []
-    keys = []
     for i, prog in enumerate(subprograms):
-        keys.append(find_key(prog))
-        om = dict(run_subprogram(prog, z_values))
+        fastprog = extract_fast_subprogram(prog)
+        om = dict(run_fast_subprogram(fastprog, z_values))
+        # om = dict(run_subprogram(prog, z_values))
         omaps.append(om)
         z_values = set(z for (_, z) in om.items() if abs(z) < 26**4)
-        # print(z_values)
-        # print(i, len(om), len(z_values))
 
     z_cands = []
     z_set = {0}
@@ -152,46 +183,23 @@ def part_1(text):
     digits = []
     for om in z_cands:
         d, z = max((d, z_) for ((d, z0), z_) in om.items() if z0 == z)
-        print(d, z)
         digits.append(d)
-
-    print(digits)
-    print("".join(str(x) for x in digits))
-
-    z = 0
-    digits = []
-    for om in z_cands:
-        d, z = min((d, z_) for ((d, z0), z_) in om.items() if z0 == z)
-        print(d, z)
-        digits.append(d)
-
-    print(digits)
-    print("".join(str(x) for x in digits))
-
-    # print(z_cands)
-    # vals = []
-    # for om in omaps:
-    #     best = max(z_cands)[0]
-    #     vals.append(best)
-    #     z_set = [z for (v, z) in v == best]
-    #     z_cands = [k for k in om.items() if k]
-    # z_values = list(range(-1000, 1001))
-    # om = list(run_subprogram(subprograms[-1], z_values))
-    # # print([x for x in om if x[-1] == 0])
-    # allowed = set([x[0][1] for x in om if x[-1] == 0])
-    # print(sorted(allowed))
-
-    # z_values = list(range(-1000, 1001))
-    # om = list(run_subprogram(subprograms[-2], z_values))
-
-    # # print([x for x in om if x[-1] == 0])
-    # print(set([x[0][1] for x in om if x[-1] in allowed]))
+    return int("".join(str(x) for x in digits))
 
 
 def part_2(text):
     """
     >>> part_2(EXAMPLE_TEXT)
+    16931171414113
     """
+    global z_cands
+    z = 0
+    digits = []
+    for om in z_cands:
+        d, z = min((d, z_) for ((d, z0), z_) in om.items() if z0 == z)
+        digits.append(d)
+
+    return int("".join(str(x) for x in digits))
 
 
 if __name__ == "__main__":
