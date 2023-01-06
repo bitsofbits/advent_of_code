5ea2d4a299754ac177928066942058922f8f4ec6
Tim Hochberg <bitsofbits@users.noreply.github.com>
2023-01-06 16:19:18 -0700

    2021, day 25: speed up to *almost* 1s run time

--- 2021/day_25/pythonimp/__main__.py
+++ 2021/day_25/pythonimp/__main__.py
@@ -1,7 +1,7 @@
 import sys
 from time import perf_counter
 
-from implementation import part_1, part_2
+from implementation import part_1
 
 if __name__ == "__main__":
     args = sys.argv[1:]
@@ -13,7 +13,3 @@ if __name__ == "__main__":
     t0 = perf_counter()
     value = part_1(text)
     print(f"Part 1: : {value} ({perf_counter() - t0:.2f}s)")
-
-    t0 = perf_counter()
-    value = part_2(text)
-    print(f"Part 2: : {value} ({perf_counter() - t0:.2f}s)")
