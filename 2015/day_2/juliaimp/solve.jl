dc12d8ae0c0bf5e2895e6166d2f9189d84213fbe
Tim Hochberg <bitsofbits@users.noreply.github.com>
2023-01-08 08:27:33 -0700

    2015, day 2: working julia version

--- 2015/day_2/juliaimp/solve.jl
+++ 2015/day_2/juliaimp/solve.jl
@@ -1,13 +1,39 @@
 
-function process_line(line)
-    (L, W, H) = map((x) -> parse(UInt128, x), split(strip(line), "x"))
-    return 2 * (L*W + W*H + H * L)
+function process_line_pt1(line)
+    (L, W, H) = map((x) -> parse(Int, x), split(strip(line), "x"))
+    faces = [L * W, W * H, H * L]
+
+    return 2 * sum(faces) + minimum(faces)
 end
+
+@assert process_line_pt1("2x3x4") == 58
+@assert process_line_pt1("1x1x10") == 43
 
 function part_1(text)
     total = 0
     for line in split(problem_text, "\n")
-        total += process_line(line)
+        total += process_line_pt1(line)
+    end
+    return total
+end
+
+
+function process_line_pt2(line)
+    (L, W, H) = map((x) -> parse(Int, x), split(strip(line), "x"))
+    half_perimeters = [L + W, W + H, H + L]
+    volume = L * W * H
+    return 2 * minimum(half_perimeters) + volume
+end
+
+
+@assert process_line_pt2("2x3x4") == 34
+@assert process_line_pt2("1x1x10") == 14
+
+
+function part_2(text)
+    total = 0
+    for line in split(problem_text, "\n")
+        total += process_line_pt2(line)
     end
     return total
 end
@@ -20,4 +46,5 @@ end
 pt1 = part_1(problem_text)  # -> 1440710 is too low!
 println("part_1: $pt1")
 
-
+pt2 = part_2(problem_text)  # -> 1440710 is too low!
+println("part_2: $pt2")
