f011047fdcf5de3bc5fbd50e29ec06ba6be7be39
Tim Hochberg <bitsofbits@users.noreply.github.com>
2023-01-09 06:16:33 -0700

    2015, day 6: working solution in Julia

--- 2015/day_6/juliaimp/solve.jl
+++ 2015/day_6/juliaimp/solve.jl
@@ -1,10 +1,69 @@
 
+function parse_point(chunk)
+    # Translate to 1 based indexing here
+    return map((x) -> parse(Int, x) + 1, split(strip(chunk), ","))
+end
+
+function parse_line(line)
+    line = strip(line)
+    chunks = split(line)
+    if length(chunks) == 5
+        chunks = chunks[2:end]
+    end
+    cmd = chunks[1]
+    p1 = parse_point(chunks[2])
+    p2 = parse_point(chunks[4])
+    return (cmd, p1, p2)
+end
+
+
 function part_1(text)
-    return 0
+    lights = zeros(Bool, 1000, 1000)
+    for line in split(strip(text), "\n")
+        cmd, (x1, y1), (x2, y2) = parse_line(line)
+        if cmd == "on"
+            lights[x1:x2, y1:y2] .= true
+        elseif cmd == "off"
+            lights[x1:x2, y1:y2] .= false
+        elseif cmd == "toggle"
+            lights[x1:x2, y1:y2] = .!lights[x1:x2, y1:y2]
+        else
+            error("unexpected command: $cmd")
+        end
+    end
+    count = 0
+    for i in 1:1000
+        for j in 1:1000
+            if lights[i, j]
+                count += 1
+            end
+        end
+    end
+    return count
 end
 
 function part_2(text)
-    return 0
+    lights = zeros(Int, 1000, 1000)
+    for line in split(strip(text), "\n")
+        cmd, (x1, y1), (x2, y2) = parse_line(line)
+        if cmd == "on"
+            lights[x1:x2, y1:y2] = lights[x1:x2, y1:y2] .+ 1
+        elseif cmd == "off"
+            mask = lights[x1:x2, y1:y2] .> 0
+            lights[x1:x2, y1:y2] = lights[x1:x2, y1:y2] - (1 .* mask)
+        elseif cmd == "toggle"
+            lights[x1:x2, y1:y2] = lights[x1:x2, y1:y2] .+ 2
+        else
+            error("unexpected command: $cmd")
+        end
+    end
+    count = 0
+    for i in 1:1000
+        for j in 1:1000
+            count += lights[i, j]
+        end
+    end
+    return count
 end
 
 
