fe1a797f666f610dbe7b6ae3e314c342e2ef1262
Tim Hochberg <bitsofbits@users.noreply.github.com>
2023-01-11 11:09:39 -0700

    2015: remove extraneous comments from template

--- 2015/template/juliaimp/solve.jl
+++ 2015/template/juliaimp/solve.jl
@@ -12,8 +12,8 @@ problem_text = open(ARGS[1],"r") do f
     strip(read(f, String))
 end
 
-pt1 = part_1(problem_text)  # -> 1440710 is too low!
+pt1 = part_1(problem_text)
 println("part_1: $pt1")
 
-pt2 = part_2(problem_text)  # -> 1440710 is too low!
+pt2 = part_2(problem_text)
 println("part_2: $pt2")
