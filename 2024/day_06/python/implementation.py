1605ffa33a2bfd1849233144c45f32ab19ca1a8f
Tim Hochberg <bitsofbits@users.noreply.github.com>
2024-12-06 15:04:57 -0700

    speed up to get under 1s

--- 2024/day_06/python/implementation.py
+++ 2024/day_06/python/implementation.py
@@ -32,6 +32,19 @@ unit_vectors = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}
 clockwise = {k: v for (k, v) in zip('^>v<', '>v<^')}
 
 
+def make_edge_states(shape):
+    m, n = shape
+    states = {}
+    for d in '^>v<':
+        for i in range(m):
+            states[(i, -1, d)] = 'exited'
+            states[(i, n, d)] = 'exited'
+        for j in range(n):
+            states[(-1, j, d)] = 'exited'
+            states[(m, j, d)] = 'exited'
+    return states
+
+
 def move(guard, board):
     i, j, d = guard
     di, dj = unit_vectors[d]
@@ -43,19 +56,18 @@ def move(guard, board):
         return (i1, j1, d)
 
 
-def find_path(board, shape, guard):
-    states = set()
+def find_path(board, shape, guard, states=None):
+    path = []
+    if states is None:
+        states = make_edge_states(shape)
+    else:
+        states = states.copy()
     while True:
-        if not on_board(guard, shape):
-            status = 'exited'
-            break
         if guard in states:
-            status = 'looped'
-            break
-        states.add(guard)
+            return path, states[guard]
+        path.append(guard)
+        states[guard] = 'looped'
         guard = move(guard, board)
-    path = {x[:2] for x in states}
-    return path, status
 
 
 def part_1(text):
@@ -65,7 +77,25 @@ def part_1(text):
     """
     board, shape, guard = parse(text)
     path, _ = find_path(board, shape, guard)
-    return len(path)
+    return len({x[:2] for x in path})
+
+
+def find_path_fast(board, shape, guard, states):
+    # Find the path, but don't track path for extra speed
+    states = states.copy()
+    i, j, d = guard
+    cw = clockwise
+    while True:
+        guard = (i, j, d)
+        if guard in states:
+            return states[guard]
+        states[guard] = 'looped'
+        di, dj = unit_vectors[d]
+        i1, j1 = (i + di, j + dj)
+        if (i1, j1) in board:
+            d = cw[d]
+        else:
+            i, j = i1, j1
 
 
 def part_2(text):
@@ -74,12 +104,20 @@ def part_2(text):
     6
     """
     board, shape, guard = parse(text)
-    default_path, _ = find_path(board, shape, guard)
+    path, _ = find_path(board, shape, guard)
     loop_count = 0
-    for x in default_path:
-        _, status = find_path(board | {x}, shape, guard)
-        if status == 'looped':
-            loop_count += 1
+    states = make_edge_states(shape)
+    tried = set()
+    last_x = path[0]
+    for i, x in enumerate(path[1:]):
+        location = x[:2]
+        if location in tried:
+            continue
+        tried.add(location)
+        status = find_path_fast(board | {location}, shape, last_x, states)
+        states[last_x] = 'looped'
+        loop_count += status == 'looped'
+        last_x = x
     return loop_count
 
 
