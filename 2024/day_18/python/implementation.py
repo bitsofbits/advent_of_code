86147d54760b34dbcf64d290f0dd6a96a6dc2777
Tim Hochberg <bitsofbits@users.noreply.github.com>
2024-12-19 16:03:18 -0700

    speed up day 18 to acceptable speed

--- 2024/day_18/python/implementation.py
+++ 2024/day_18/python/implementation.py
@@ -1,14 +1,15 @@
 from heapq import heappop, heappush
 from itertools import count
 
+
 def parse(text):
     """
     >>> parse(EXAMPLE_TEXT)[:3]
     [(4, 5), (2, 4), (5, 4)]
     """
     coords = []
-    for row in text.strip().split('\n'):
-        x, y = (int(x) for x in row.split(','))
+    for row in text.strip().split("\n"):
+        x, y = (int(x) for x in row.split(","))
         coords.append((y, x))
     return coords
 
@@ -24,11 +25,11 @@ def render(board):
         row = []
         for j in range(min_j, max_j + 1):
             if (i, j) in board:
-                row.append('#')
+                row.append("#")
             else:
-                row.append('.')
-        rows.append(''.join(row))
-    return '\n'.join(rows)
+                row.append(".")
+        rows.append("".join(row))
+    return "\n".join(rows)
 
 
 def create_board(coords, size):
@@ -47,7 +48,7 @@ def create_board(coords, size):
     #########
     """
     board = {xy for xy in coords}
-    for i in range(-1, size+1):
+    for i in range(-1, size + 1):
         board.add((i, -1))
         board.add((i, size))
         board.add((-1, i))
@@ -55,13 +56,14 @@ def create_board(coords, size):
     return board
 
 
-def find_path_steps(board, start, end):
-    queue = [(0, start)]
+def find_path(board, start, end):
+    queue = [(0, start, ())]
     seen = set()
     while queue:
-        steps, location = heappop(queue)
+        steps, location, path = heappop(queue)
+        path += (location,)
         if location == end:
-            return steps
+            return path
         if location in seen:
             continue
         seen.add(location)
@@ -70,8 +72,7 @@ def find_path_steps(board, start, end):
         for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             next_location = (i + di, j + dj)
             if next_location not in board:
-                heappush(queue, (next_steps, next_location))
-
+                heappush(queue, (next_steps, next_location, path))
 
 
 def part_1(text, size=71, steps=1024):
@@ -81,24 +82,46 @@ def part_1(text, size=71, steps=1024):
     """
     coords = parse(text)
     board = create_board(coords[:steps], size)
-    return find_path_steps(board, (0, 0), (size - 1, size - 1))
+    return len(find_path(board, (0, 0), (size - 1, size - 1))) - 1
 
+
+# def find_all_paths(board, start, end):
+#     queue = [(0, start, ())]
+#     seen = set()
+#     while queue:
+#         steps, location, path = heappop(queue)
+#         path += (location,)
+#         if location == end:
+#             yield path
+#         key = frozenset(path)
+#         if key in seen:
+#             continue
+#         seen.add(key)
+#         i, j = location
+#         next_steps = steps + 1
+#         for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+#             next_location = (i + di, j + dj)
+#             if next_location not in board:
+#                 heappush(queue, (next_steps, next_location, path))
 
 
 def part_2(text, size=71, initial_steps=1024):
     """
     >>> print(part_2(EXAMPLE_TEXT, size=7, initial_steps=12))
     6,1
-
-    46,16 wrong
     """
     coords = parse(text)
-    for steps in count(initial_steps + 1):
-        board = create_board(coords[:steps], size)
-        escape_count = find_path_steps(board, (0, 0), (size - 1, size - 1))
-        if escape_count is None:
-            i, j = coords[steps - 1]
-            return f"{j},{i}"
+    start = (0, 0)
+    end = (size - 1, size - 1)
+    board = create_board(coords[:initial_steps], size)
+    path = find_path(board, start, end)
+    for location in coords[initial_steps:]:
+        board.add(location)
+        if location in path:
+            path = find_path(board, start, end)
+            if path is None:
+                i, j = location
+                return f"{j},{i}"
 
 
 if __name__ == "__main__":
