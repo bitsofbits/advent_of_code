6f3a1a8fd1fadb5718e29168e26bd3de284d383f
Tim Hochberg <bitsofbits@users.noreply.github.com>
2024-12-20 12:19:35 -0700

    get day 20 down to under 1s

--- 2024/day_20/python/implementation.py
+++ 2024/day_20/python/implementation.py
@@ -69,8 +69,8 @@ def part_1(text, return_detailed_counts=False, min_delta=100):
     [(2, 14), (4, 14), (6, 2), (8, 4), (10, 2), (12, 3), (20, 1), (36, 1), (38, 1), (40, 1), (64, 1)]
 
     >>> part_2(EXAMPLE_TEXT, True, 1, max_cheat=2)
-    [(2, 14), (4, 14), (6, 2), (8, 4), (10, 2), (12, 3), (20, 1), (36, 1), (38, 1), (40, 1), (64, 1)]
-
+    44
+    
     """
     start, end, walls = parse(text)
     time_from_start = find_min_time_from(start, walls)
@@ -103,67 +103,46 @@ def part_1(text, return_detailed_counts=False, min_delta=100):
 
 def part_2(text, return_detailed_counts=False, min_delta=100, max_cheat=20):
     """
-    >>> part_2(EXAMPLE_TEXT, True, 50)[:4]
-    [(50, 32), (52, 31), (54, 29), (56, 39)]
+    >>> part_2(EXAMPLE_TEXT, True, 50)
+    285
     """
     start, end, walls = parse(text)
     time_from_start = find_min_time_from(start, walls)
     time_without_cheating = time_from_start[end] 
     time_from_end = find_min_time_from(end, walls)
-    # Cheating 2ps is equivalent to deleting one wall
-    delta_times = []
+    count = 0
     min_i = min(i for (i, j) in walls)
     max_i = max(i for (i, j) in walls)
     min_j = min(j for (i, j) in walls)
     max_j = max(j for (i, j) in walls)
-    seen = set()
+    max_time = time_without_cheating - min_delta
+    offsets = list(range(-max_cheat, max_cheat + 1))
     for i0 in range(min_i, max_i + 1):
         for j0 in range(min_j, max_j + 1):
             p0 = (i0, j0)
             if p0 in walls:
                 continue
-            for di in range(-max_cheat, max_cheat + 1):
-                for dj in range(-max_cheat, max_cheat + 1):
-                    distance = abs(di) + abs(dj)
+            t0 = time_from_start[p0]
+            if t0 > max_time:
+                continue
+            max_time_ij = max_time - t0
+            for di in offsets:
+                abs_di = abs(di)
+                i1 = i0 + di
+                for dj in offsets:
+                    distance = abs_di + abs(dj)
                     if distance > max_cheat:
                         continue
-                    p1 = (i1, j1) = (i0 + di, j0 + dj)
+                    p1 = (i1, j0 + dj)
                     if p1 in walls:
                         continue
-                    if (p0, p1) in seen:
-                        continue
-                    seen.add((p0, p1))
-                    t = time_from_start.get(p0, inf) + time_from_end.get(p1, inf) + distance
-                    delta = time_without_cheating - t
-                    if delta >= min_delta:
-                        delta_times.append(delta)
+                    t1 = time_from_end.get(p1, inf) + distance
+                    if t1 <= max_time_ij:
+                        count += 1
 
-    if return_detailed_counts:
-        return sorted(Counter(delta_times).most_common())
-    else:
-        return len(delta_times)    
+    return count
 
 
-
-    # for wall in walls:
-
-    #     i, j = wall
-    #     for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-    #         p1 = (i + di, j + dj)
-    #         if p1 in walls:
-    #             continue
-    #         for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-    #             p2 = (i + di, j + dj)
-    #             if p2 in walls:
-    #                 continue
-    #             t = time_from_start.get(p1, inf) + time_from_end.get(p2, inf) + 2
-    #             delta = time_without_cheating - t
-    #             if delta >= min_delta:
-    #                 delta_times.append(delta)
-    # if return_detailed_counts:
-    #     return Counter(delta_times).most_common()
-    # else:
-    #     return len(delta_times)
 
 
 if __name__ == "__main__":
