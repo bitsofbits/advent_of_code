3102fb478463b22ea0292d0013780fb4f7625e5f
Tim Hochberg <bitsofbits@users.noreply.github.com>
2024-12-22 14:45:51 -0700

    shave another 0.04 s off of day22

--- 2024/day_22/python/implementation.py
+++ 2024/day_22/python/implementation.py
@@ -10,10 +10,11 @@ def parse(text):
     return [int(x) for x in text.strip().split("\n")]
 
 
-def generate_numbers(x, n):
+@cache
+def list_numbers(x, n):
     """
     >>> x = 123
-    >>> for x in generate_numbers(x, 10): print(x)
+    >>> for x in list_numbers(x, 10): print(x)
     15887950
     16495136
     527345
@@ -25,16 +26,15 @@ def generate_numbers(x, n):
     7753432
     5908254
     """
+    numbers = []
     for _ in range(n):
         x = (x ^ (x << 6)) % 16777216
-        x = x ^ (x >> 5)  # % 16777216, don't need this because we are dividing and already smaller than 
+        x = (
+            x ^ (x >> 5)
+        )  # % 16777216, don't need this because we are dividing and already smaller than
         x = (x ^ (x << 11)) % 16777216
-        yield x
-
-
-@cache
-def list_numbers(x, n):
-    return list(generate_numbers(x, n))
+        numbers.append(x)
+    return numbers
 
 
 def part_1(text):
@@ -56,24 +56,25 @@ def part_2(text):
     1405
     """
     prices_per_pattern = defaultdict(int)
+    key_size = 19
+    min_key_value = key_size**3
     for x in parse(text):
         seen = set()
         last_price = x % 10
         key = 0
-        max_key = 20**4
         for x in list_numbers(x, 2000):
             price = x % 10
             price_change = price - last_price
             last_price = price
-            key = (key * 20) % max_key + (price_change + 10)
+            key = (key % min_key_value) * key_size + (price_change + 10)
             if key in seen:
                 continue
             seen.add(key)
             prices_per_pattern[key] += price
-
-    min_key_value = 20**3
     return max(v for (k, v) in prices_per_pattern.items() if k >= min_key_value)
 
+
+print(2147483647, 20**4)
 
 if __name__ == "__main__":
     import doctest
