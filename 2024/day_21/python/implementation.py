43754ec35313b923ec960a66cec7c4c69bccef73
Tim Hochberg <bitsofbits@users.noreply.github.com>
2024-12-22 10:33:23 -0700

    do a little cleanup, although still kind of convoluted

--- 2024/day_21/python/implementation.py
+++ 2024/day_21/python/implementation.py
@@ -3,47 +3,44 @@ from itertools import pairwise
 from collections import Counter
 from math import inf
 
+
 def parse(text):
     """
     >>> parse(EXAMPLE_TEXT)
     ['029A', '980A', '179A', '456A', '379A']
     """
-    return text.strip().split('\n')
+    return text.strip().split("\n")
+
 
 door = {
- (0, 0) : '7',
- (0, 1) : '8',
- (0, 2) : '9',
- (1, 0) : '4',
- (1, 1) : '5',
- (1, 2) : '6',
- (2, 0) : '1',
- (2, 1) : '2',
- (2, 2) : '3',
- (3, 1) : '0',
- (3, 2) : 'A'
+    (0, 0): "7",
+    (0, 1): "8",
+    (0, 2): "9",
+    (1, 0): "4",
+    (1, 1): "5",
+    (1, 2): "6",
+    (2, 0): "1",
+    (2, 1): "2",
+    (2, 2): "3",
+    (3, 1): "0",
+    (3, 2): "A",
 }
 
-robot = {
-    (0, 1) : '^',
-    (0, 2) : 'A',
-    (1, 0) : '<',
-    (1, 1) : 'v',
-    (1, 2) : '>'
-}
+robot = {(0, 1): "^", (0, 2): "A", (1, 0): "<", (1, 1): "v", (1, 2): ">"}
 
 
 unit_vectors = {">": (0, 1), "v": (1, 0), "<": (0, -1), "^": (-1, 0)}
+
 
 def find_paths_between(start, end, keymap):
     """
     >>> list(find_paths_between('7', 'A', door))[:4]
     ['>v>vv', '>>vvv', '>vvv>', 'vv>v>']
     """
-    inv_map = {v : k for (k, v) in keymap.items()}
+    inv_map = {v: k for (k, v) in keymap.items()}
     start = inv_map[start]
     end = inv_map[end]
-    queue = [(0, start, frozenset(), '')]
+    queue = [(0, start, frozenset(), "")]
     while queue:
         count, loc, seen, path = heappop(queue)
         if loc == end:
@@ -57,6 +54,7 @@ def find_paths_between(start, end, keymap):
             next_loc = (i0 + di, j0 + dj)
             if next_loc in keymap:
                 heappush(queue, (count, next_loc, seen, path + k))
+
 
 door_paths = {}
 for start in door:
@@ -73,8 +71,7 @@ for start in robot:
         robot_paths[(start, end)] = list(find_paths_between(start, end, robot))
 
 robot_costs = {}
-robot_costs[0] = {k : 1 for k in robot_paths}
-
+robot_costs[0] = {k: 1 for k in robot_paths}
 
 
 def compute_robot_costs(cost_values):
@@ -83,80 +80,40 @@ def compute_robot_costs(cost_values):
     for k in robot_paths:
         best_cost = inf
         for path in robot_paths[k]:
-            transistions = list(pairwise('A' + path + 'A'))
+            transistions = list(pairwise("A" + path + "A"))
             best_cost = min(best_cost, sum(cost_values[x] for x in transistions))
         costs[k] = best_cost
     return costs
 
+
 for i in range(25):
-    robot_costs[i + 1] = compute_robot_costs(robot_costs[i]) 
+    robot_costs[i + 1] = compute_robot_costs(robot_costs[i])
 
 
-
-def compute_top_level_pushes(pushes, path_maps, push=False, cache=None):
+def compute_cost(pushes, n_robots):
     """
-    >>> compute_top_level_pushes('029A', [door_paths])
-    '<A^A>^^AvvvA'
-
-    >>> Counter(compute_top_level_pushes('029A', [door_paths, robot_paths, robot_paths])).most_common()
-    [('A', 28), ('<', 11), ('>', 11), ('v', 9), ('^', 9)]
-
-    >>> compute_top_level_pushes('029A', [door_paths, robot_paths])
-    '<v<A>^>A<A>AvA^<AA>A<vAAA^>A'
-
-    >>> compute_top_level_pushes('029A', [door_paths, robot_paths, robot_paths])
-    '<vA<AA>^>AvAA^<A>A<v<A>^>AvA^A<vA^>A<Av<A>^>AAvA^A<v<A>A^>AAA<A>vA^A'
-
-    >>> compute_top_level_pushes('v<<A>>^A<A>AvA<^AA>A<vAAA>^A', [robot_paths])
-    '<vA<AA>^>AvAA^<A>A<v<A>^>AvA^A<vA^>A<v<A>^A>AAvA^A<v<A>A^>AAAvA^<A>A'
-
-    """
-    if push:
-        pushes += 'A'
-
-    if not path_maps:
-        return pushes
-    # assert pushes[-1] == 'A'
-    path_map = path_maps[0]
-    path_maps = tuple(path_maps[1:])
-    transistions = list(pairwise('A' + pushes))
-    path = ''
-    for x in transistions:
-        best_sub_path = None
-        for sub_pushes in path_map[x]:
-            sub_path = compute_top_level_pushes(sub_pushes, path_maps, 
-                                                push=True, cache=cache)
-            if best_sub_path is None or len(sub_path) < len(best_sub_path):
-                best_sub_path = sub_path
-        path += best_sub_path
-    return path
-
-
-def compute_top_level_pushes_2(pushes, n_robots):
-    """
-    >>> compute_top_level_pushes_2('029A', 0)
+    >>> compute_cost('029A', 0)
     12
 
-    >>> compute_top_level_pushes_2('029A', 1)
+    >>> compute_cost('029A', 1)
     28
 
-    >>> compute_top_level_pushes_2('029A', 2)
+    >>> compute_cost('029A', 2)
     68
 
     Check that it's fast enough
-    >>> _ = compute_top_level_pushes_2('029A', 25)
+    >>> _ = compute_cost('029A', 25)
     """
-    transistions = list(pairwise('A' + pushes))
+    transistions = list(pairwise("A" + pushes))
     costs = robot_costs[n_robots]
     cost = 0
     for x in transistions:
         subpath_cost = inf
         for subpath in door_paths[x]:
-            sub_trans = list(pairwise('A' + subpath + 'A'))
+            sub_trans = list(pairwise("A" + subpath + "A"))
             subpath_cost = min(subpath_cost, sum(costs[y] for y in sub_trans))
         cost += subpath_cost
     return cost
-
 
 
 def part_1(text, n_robots=2):
@@ -167,13 +124,15 @@ def part_1(text, n_robots=2):
     codes = parse(text)
     score = 0
     for code in codes:
-        value = int(code.lstrip('0')[:-1])
-        count = compute_top_level_pushes_2(code, n_robots)
+        value = int(code.lstrip("0")[:-1])
+        count = compute_cost(code, n_robots)
         score += count * value
     return score
 
+
 def part_2(text):
     return part_1(text, n_robots=25)
+
 
 if __name__ == "__main__":
     import doctest
