42e2648599af27052cdedb808a398ae459fd0c15
Tim Hochberg <bitsofbits@users.noreply.github.com>
2024-12-25 10:36:40 -0700

    working day_24 version

--- 2024/day_24/python/implementation.py
+++ 2024/day_24/python/implementation.py
@@ -1,6 +1,6 @@
-from itertools import combinations, product
-from typing import NamedTuple, Literal, Any
-from functools import cache
+from itertools import combinations,  product
+from typing import Literal, Any
+from dataclasses import dataclass
 
 def parse(text):
     """
@@ -10,69 +10,83 @@ def parse(text):
     >>> gates
     {'z00': ('x00', 'AND', 'y00'), 'z01': ('x01', 'XOR', 'y01'), 'z02': ('x02', 'OR', 'y02')}
     """
-    val_text, gate_text = text.strip().split('\n\n')
+    val_text, gate_text = text.strip().split("\n\n")
     values = {}
-    for row in val_text.strip().split('\n'):
-        key, val = row.split(': ')
+    for row in val_text.strip().split("\n"):
+        key, val = row.split(": ")
         values[key] = int(val)
     gates = {}
-    for row in gate_text.strip().split('\n'):
+    for row in gate_text.strip().split("\n"):
         k1, op, k2, _, key = row.strip().split()
         gates[key] = (k1, op, k2)
 
     return values, gates
 
 
-
-class Value(NamedTuple):
+@dataclass
+class Value:
     """
-    >>> Value(name='x3').evaluate((1, 0, 1, 1), (0, 1, 0, 1))
+    >>> Value(name='x3').evaluate(values={'x3' : 1})
     1
     """
-    name : str
 
-    @cache
-    def evaluate(self, x, y):
-        i = int(self.name[1:])
-        match self.name[0]:
-            case 'x':
-                return x[i]
-            case 'y':
-                return y[i]
-            case _:
-                raise ValueError(self.name)
+    name: str
 
-class Operation(NamedTuple):
+    def evaluate(self, values, skip_overrides=False):
+        x = values[self.name]
+        if isinstance(x, (Operation, Value)):
+            return x.evaluate(values)
+        else:
+            return x
+        
+
+
+@dataclass
+class Operation:
     """
     >>> a = Value('x3')
     >>> b = Value('y3')
-    >>> op = Operation(a, b, 'XOR')
-    >>> op.evaluate((1, 0, 1, 1), (0, 1, 0, 1))
+    >>> op = Operation('op1', a, b, 'XOR')
+    >>> op.evaluate(values={'x3': 1, 'y3' : 1})
     0
-    """
-    a : Any
-    b : Any
-    op : Literal['AND', 'OR', 'XOR']
 
-    @cache
-    def evaluate(self, x, y):
-        a = self.a.evaluate(x, y)
-        b = self.b.evaluate(x, y)
+    # >>> c = Value('y0')
+    # >>> op.evaluate((1, 0, 1, 1), (0, 1, 0, 1), swaps=frozenset([('y3', c)]))
+    # 1
+    """
+
+    name: str
+    a: Any
+    b: Any
+    op: Literal["AND", "OR", "XOR"]
+
+    def evaluate(self, values=None, skip_overrides=False):
+        x = values.get(self.name)
+        if x is not None and not skip_overrides:
+            if isinstance(x, (Operation, Value)):
+                return x.evaluate(values, skip_overrides=True)
+            else:
+                return x
+        a = self.a.evaluate(values)
+        b = self.b.evaluate(values)
         match self.op:
-            case 'AND':
+            case "AND":
                 return a & b
-            case 'OR':
+            case "OR":
                 return a | b
-            case 'XOR':
+            case "XOR":
                 return a ^ b
             case _:
                 raise ValueError(self.op)
+
+
+
 
 def build_op_values(gates):
     values = {}
     for k1, _, k2 in gates.values():
         for key in [k1, k2]:
-            if key[0] in 'xy':
+            if key[0] in "xy":
                 values[key] = Value(key)
     return values
 
@@ -88,224 +102,184 @@ def build_operations(gates):
             if a in processed_gates and b in processed_gates:
                 a = processed_gates[a]
                 b = processed_gates[b]
-                processed_gates[key] = Operation(a, b, op)
+                processed_gates[key] = Operation(key, a, b, op)
                 pending_deletions.add(key)
         for key in pending_deletions:
-           unprocessed_gates.pop(key)
+            unprocessed_gates.pop(key)
     return processed_gates
 
 
-def build_values(values):
-    max_xi = max_yi = 0
-    for key in values:
-        i = int(key[1:])
-        if key[0] == 'x':
-            max_xi = max(max_xi, i)
-        elif key[0] == 'y':
-            max_yi = max(max_yi, i)
-        else:
-            raise ValueError(key)
-    x_values = [None] * (max_xi + 1)
-    y_values = [None] * (max_yi + 1)
-    for key, v in values.items():
-        i = int(key[1:])
-        if key[0] == 'x':
-            x_values[i] = v
-        elif key[0] == 'y':
-            y_values[i] = v
-        else:
-            raise ValueError(key)
-    return tuple(x_values), tuple(y_values)  
-
-
-
-def compute(gate, values):
-    k1, op, k2 = gate
-    if op == 'AND':
-        return values[k1] & values[k2]
-    elif op == 'OR':
-        return values[k1] | values[k2]
-    elif op == 'XOR':
-        return values[k1] ^ values[k2]
-    else:
-        raise ValueError(op)
-
-
-
-
-def compute_dependencies_of(key, gates):
-    pending = [key]
-    dependencies = set()
-    while pending:
-        k = pending.pop()
-        if k in dependencies:
-            continue
-        dependencies.add(k)
-        if k[0] in 'xy':
-            continue
-        a, _, b = gates[k]
-        pending.append(a)
-        pending.append(b)
-    return dependencies
-
-
-def compute_raw_output(values, gates):
-    values = values.copy()
-    gates = gates.copy()
-    while gates:
-        completed = set()
-        for key, gate in gates.items():
-            try:
-                values[key] = compute(gate, values)
-            except KeyError as err:
-                if key.startswith('x') or key.startswith('y'):
-                    print(err)
-                pass
-            else:
-                completed.add(key)
-        for k in completed:
-            gates.pop(k)
-    return {k : v for (k, v) in values.items() if k.startswith('z')}
+# def build_values_map(values):
+#     map_ = {}
+#     for key, v in values.items():
+#         i = int(key[1:])
+#         if key[0] == 'x':
+#             map
+#         elif key[0] == 'y':
+#             y_values[i] = v
+#         else:
+#             raise ValueError(key)
+#     return tuple(x_values), tuple(y_values)
 
 
 def compute_output(operations, values):
     output = 0
     for k in reversed(sorted(operations)):
-        if k.startswith('z'):
-            output = (output << 1) + operations[k].evaluate(*values)
+        if k.startswith("z"):
+            output = (output << 1) + operations[k].evaluate(values)
     return output
 
-    # values = compute_raw_output(values, gates)
-
-    # output = 0
-    # for k in reversed(sorted(values)):
-    #     if k.startswith('z'):
-    #         output = (output << 1) + values[k]
-    # return output    
 
 def part_1(text):
     """
     >>> part_1(EXAMPLE2_TEXT)
     2024
     """
-    raw_values, gates = parse(text)
+    values, gates = parse(text)
     operations = build_operations(gates)
-    values = build_values(raw_values)
-
-
     return compute_output(operations, values)
 
 
-
-def bad_bits(gates, bits):
-    values = {}
-    for i in range(bits):
-        values[f'x{i:02d}'] = values[f'y{i:02d}'] = False
-
-    for i in range(bits - 1):
-        test_values = values.copy()
-        for x in [False, True]:
-            for y in [False, True]:
-                test_values[f'x{i:02d}'] = x
-                test_values[f'y{i:02d}'] = y
-                outputs = compute_raw_output(test_values, gates)
-                if outputs[f'z{i:02d}'] != x ^ y:
-                    yield f'z{i:02d}'
-                if outputs[f'z{i + 1:02d}'] != x & y:
-                    yield f'z{i:02d}'
+def build_half_adder(i, x=None, y=None, prefix=""):
+    if x is None:
+        x = Value(name=f"x{i:02d}")
+    if y is None:
+        y = Value(name=f"y{i:02d}")
+    sum_ = Operation(prefix + f"sum_{i}", x, y, "XOR")
+    carry = Operation(prefix + f"carry{i}", x, y, "AND")
+    return sum_, carry
 
 
-def is_valid(gates, bits, keys=None):
-    for x in bad_bits(gates, bits):
-        if keys is None or x in keys:
+def build_adder(i, carry):
+    half_sum, half_carry = build_half_adder(i, prefix="half_")
+    sum_ = Operation(f"sum_{i}", half_sum, carry, "XOR")
+    carry = Operation(
+        f"carry_{i}", half_carry, Operation(f"merge_{i}", carry, half_sum, "AND"), "OR"
+    )
+    return sum_, carry
+
+
+def equivalent_over(keys, op1, op2):
+    # TODO: also check size?
+    series = [[(k, 0), (k, 1)] for k in keys]
+    for items in product(*series):
+        values = dict(items)
+        try:
+            if op1.evaluate(values) != op2.evaluate(values):
+                return False
+        except KeyError:
             return False
     return True
-
-
-def is_valid_and(gates, bits, keys=None):
-    values = {}
-    for i in range(bits):
-        values[f'x{i:02d}'] = values[f'y{i:02d}'] = False
-
-    for i in range(bits):
-        test_values = values.copy()
-        key_z = f'z{i:02d}'
-        if key_z not in keys:
+    
+def extract_ops_down_to(terminal_keys, op):
+    pending = [op]
+    ops = []
+    while pending:
+        op = pending.pop()
+        if op in ops or op.name in terminal_keys:
             continue
-        for x in [False, True]:
-            for y in [False, True]:
-                test_values[f'x{i:02d}'] = x
-                test_values[f'y{i:02d}'] = y
-                outputs = compute_raw_output(test_values, gates)
-                if outputs[key_z] != x & y:
-                    return False
-    return True
+        ops.append(op)
+        if isinstance(op, Operation):
+            pending.append(op.a)
+            pending.append(op.b)
+    return ops
+
+def extract_keys_down_to(terminal_keys, op):
+    return {x.name for x in extract_ops_down_to(terminal_keys, op)}
+
+def find_swap_target(input_names, add, operations):     
+    examples = extract_ops_down_to(input_names, add)
+    for example, candidate in product(examples, operations.values()):
+        if equivalent_over(input_names, example, candidate):
+            return candidate
 
 
-def part_2(text, swaps=4, validation=is_valid):
+def swap_ops(op_x, op_y, operations):
+    name_x = op_x.name
+    name_y = op_y.name
+
+    for k, v in operations.items():
+        if isinstance(v, Operation):
+            new_a = v.a
+            new_b = v.b
+            if v.a.name == name_x:
+                new_a = op_y
+            if v.b.name == name_x:
+                new_b = op_y
+            if v.a.name == name_y:
+                new_a = op_x
+            if v.b.name == name_y:
+                new_b = op_x
+            v.a = new_a
+            v.b = new_b
+
+    for k, v in operations.items():
+        if v.name == name_x:
+            v.name = name_y
+        elif v.name == name_y:
+            v.name = name_x
+
+    operations[name_x], operations[name_y] = operations[name_y], operations[name_x]
+    for name in [name_x, name_y]:
+        assert operations[name].name == name
+
+    for k, v in operations.items():
+        assert v.name == k
+
+
+
+
+def part_2(text, swaps=4):
     """
-    >>> part_2(EXAMPLE3_TEXT, swaps=2, validation=is_valid_and)
+    >>> part_2(DATA_TEXT)
     """
-    # values, gates = parse(text)
-    # bits = sum(1 for x in gates if x.startswith('z'))
+    raw_values, gates = parse(text)
+    operations = build_operations(gates)
+    # x_len, y_len = (len(x) for x in build_values(raw_values))
+    # assert x_len == y_len
 
-    # bad_output_bits = set((bad_bits(gates, bits)))
+    add_0, carry_0 = build_half_adder(0)
 
-    # # potentially_useful_gates = set()
-    # # for i in range(bits):
-    # #     if i not in bad_output_bits:
-    # #         key = f'z{i:02d}'
-    # #         raw_candidates = compute_dependencies_of(key, gates)
-    # #         potentially_useful_gates |= raw_candidates
-
-    # candidates = {}
-    # for key in sorted(bad_output_bits):
-    #     raw_candidates = compute_dependencies_of(key, gates)
-    #     # raw_candidates = {x for x in raw_candidates if x not in useful_gates}
-    #     raw_candidates = {x for x in raw_candidates if x[0] not in 'xy'}
-    #     candidates[key] = raw_candidates
-
-    # # for key in candidates:
-    # #     print(key, ">>>", candidates[key])
-
-    # # print(len(candidates))
-
-    # swaps = set()
-    # for key_a in candidates:
-    #     print(key_a)
-    #     for key_b in candidates:
-    #         # It looks like we are probably swapping between groups
-    #         pairs = set(product(candidates[key_a], candidates[key_b]))
-    #         print(len(pairs))
-    #         for pair in pairs:
-    #             if pair in swaps:
-    #                 continue
-    #             k1, k2 = pair
-    #             if k1 == k2:
-    #                 continue
-    #             swapped_gates = gates.copy()
-    #             swapped_gates[k1], swapped_gates[k2] = swapped_gates[k2], swapped_gates[k1]
-    #             if validation(swapped_gates, bits, keys=(key_a, key_b)):
-    #                 swaps.add(frozenset(pair))
-
-    # print(swaps)
-
-    # # pairs = list(combinations(gates, 2))
-    # print(len(pairs))
-    # print(len(list(combinations(pairs, swaps))))
-    # for groups in combinations(combinations(gates, 2), swaps):
-    #     swapped_gates = gates.copy()
-    #     for k1, k2 in groups:
-    #         swapped_gates[k1], swapped_gates[k2] = swapped_gates[k2], swapped_gates[k1]
-    #     # adjust gates
-    #     if validation(swapped_gates, bits):
-    #         names = set()
-    #         for x in groups:
-    #             names |= set(x)
-    #         return ','.join(sorted(names))
+    for k, v in operations.items():
+        if equivalent_over(['x00', 'y00'], carry_0, v):
+                break
+    else:
+        raise ValueError('no carry_0 match')
+    last_carry = Value(k)
 
 
+    swapped = []
+    for i in range(1, 45):
+        add, carry = build_adder(i, last_carry)
+        key = f'z{i:02d}'
+        op = operations[key]
+        input_names = [f'x{i:02d}', f'y{i:02d}', last_carry.name]
 
+        if not equivalent_over(input_names, add, op):
+            swap_target = find_swap_target(input_names, add, operations)
+            for swap_source in [op, op.a, op.b]:
+                swap_ops(swap_source, swap_target, operations)
+                test_op = operations[key]
+                if equivalent_over(input_names, add, test_op):
+                    swapped.extend((swap_source.name, swap_target.name))
+                    break
+                swap_ops(swap_source, swap_target, operations)
+            else:
+
+                raise ValueError('no swap found for Add', i)
+    
+
+        for k, v in operations.items():
+            if equivalent_over(input_names, carry, v):
+                break
+        else:
+            print('no carry', i, 'match')
+
+        last_carry = Value(k)
+
+    return ','.join(sorted(swapped))
+
+   
 
 if __name__ == "__main__":
     import doctest
@@ -318,4 +292,6 @@ if __name__ == "__main__":
         EXAMPLE2_TEXT = f.read()
     with open(data_dir / "example3.txt") as f:
         EXAMPLE3_TEXT = f.read()
+    with open(data_dir / "data.txt") as f:
+        DATA_TEXT = f.read()
     doctest.testmod()
